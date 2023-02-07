20c5df38e1271f38ec38c0d20157faf364e3e3d2
Yochi22 <116101499+Yochi22@users.noreply.github.com>
2023-02-06 20:52:13 -0600

    creacion del servidor

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hola Mundo')
+const { response } = require('express')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => {
+res.send("Hola Mundo")
+})
+
+app.listen(port, ()=> {
+  console.log("Mi puerto es " + port)
+} )
